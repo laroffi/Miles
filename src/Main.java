04eb77aab985c72d9e0613862e57be3c0427b232
Larisa Pylaeva <laralord@yandex.ru>
2021-04-07 12:26:43 +0300

    All done

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long cost = 3503_00;
+        int bonusMile = 20;
+        long MileAmount = cost / bonusMile / 100;
+        System.out.println(MileAmount);
+    }
+}
